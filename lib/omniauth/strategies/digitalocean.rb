a10d172f7d3d9677face8812c60c1a0edcc0f9da
bryanl <bryan@osesm.com>
2015-11-17 10:02:42 -0500

    remove query string from callback_url

--- lib/omniauth/strategies/digitalocean.rb
+++ lib/omniauth/strategies/digitalocean.rb
@@ -65,6 +65,11 @@ module OmniAuth
         end
       end
 
+      # Don't include the query string in the callback_url.
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
     end
   end
 end
